a59176155da46bc2f81b55644c3c84c437e5574b
Leshawn Bradley <leshawnbradley239@gmail.com>
2025-04-03 00:04:41 -0700

    starting from scratch

---
+++ team-git-it-done-frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'team-git-it-done-frontend',
+  webDir: 'www'
+};
+
+export default config;
