455e4f1854f07976f22ab93605ae35726600725e
Medgar24 <ALANADAZE@GMAIL.COM>
2025-04-11 14:30:28 -0500

    I added a standalone: false to the user.page.ts so the server can run

--- team-git-it-done-frontend/src/app/user/user.page.ts
+++ team-git-it-done-frontend/src/app/user/user.page.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-user',
   templateUrl: './user.page.html',
   styleUrls: ['./user.page.scss'],
+  standalone: false,
 })
 export class UserPage implements OnInit {
 
