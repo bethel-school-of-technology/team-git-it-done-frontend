19f361a828c69e10afe4d66d6cba91ab52f0865b
Leshawn Bradley <leshawnbradley239@gmail.com>
2025-04-13 22:08:51 -0700

    added code to models file

--- team-git-it-done-frontend/src/app/models/bill-link.ts
+++ team-git-it-done-frontend/src/app/models/bill-link.ts
@@ -3,5 +3,5 @@ export class BillLink {
     id : number=0;
     userId : number=0;
     billId : number=0;
-    Bill : number=0;
+    bill : number=0;
 }
--- team-git-it-done-frontend/src/app/models/bill.ts
+++ team-git-it-done-frontend/src/app/models/bill.ts
@@ -1,7 +1,30 @@
+// export class Bill {
+
+//     id : number=0;
+//     name : string="";
+//     description : string="";
+//     price : number=0;
+// }
+
+//constructor was added bellow
+
 export class Bill {
 
-    id : number=0;
-    name : string="";
-    description : string="";
-    price : number=0;
+    billid? : number=0;
+    name? : "";
+    description? : string="";
+    price? : number=0;
+    creatorId? : "";
+    billLinks? : "";
+
+    constructor(billid?: number, name?: "", description?: "", price?: number, creatorId?:"", billLinks?: "") {
+        this.billid = billid;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.creatorId = creatorId;
+        this.billLinks = billLinks;
+
+    }
 }
+
