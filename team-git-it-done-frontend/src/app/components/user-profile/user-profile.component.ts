f8f448aaf3246996e3951e1e6ada2a12edc33c2a
ChrisVansingel <chrisvansingel@gmail.com>
2025-04-19 22:38:14 -0400

    Misc routing/pathing

--- team-git-it-done-frontend/src/app/components/user-profile/user-profile.component.ts
+++ team-git-it-done-frontend/src/app/components/user-profile/user-profile.component.ts
@@ -1,14 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+// import { NgModel } from '@angular/forms';
 
-@Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.scss'],
+@NgModule({
+imports: [
+CommonModule,
+IonicModule,
+],
+declarations: []
 })
-export class UserProfileComponent  implements OnInit {
+export class UserProfileComponent {}
 
-  constructor() { }
+// @Component({
+//   selector: 'app-user-profile',
+//   templateUrl: './user-profile.component.html',
+//   styleUrls: ['./user-profile.component.scss'],
+// })
+// export class UserProfileComponent  implements OnInit {
 
-  ngOnInit() {}
+//   constructor() { }
 
-}
+//   ngOnInit() {}
+
+// }
