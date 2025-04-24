81a5778059cd1236ae0a4ea2860e6fe6d3c92158
Medgar24 <ALANADAZE@GMAIL.COM>
2025-04-23 19:19:55 -0500

    fixing sign up

--- team-git-it-done-frontend/src/app/sign-up/sign-up.page.ts
+++ team-git-it-done-frontend/src/app/sign-up/sign-up.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.page.scss'],
 })
 export class SignUpPage implements OnInit {
+  newUser: User = new User();
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+  }
+
+  signUp() {
+    this.userService.signUp(this.newUser).subscribe(() => {
+        window.alert("User Registered Successfully");
+        this.router.navigate(['signin']);
+    }, error => {
+        window.alert("User Registration Error");
+        console.log('Error: ', error)
+    });
   }
 
 }
