f37cd37bb34414460b1880d291ed1658bf87bd02
Zian Jacobsen <25855271@sun.ac.za>
2025-04-27 14:37:58 +0200

    Finished Bill Editting and adding users to the split. Also added a way to get a user's details from the Service

--- team-git-it-done-frontend/src/app/services/dialog.service.ts
+++ team-git-it-done-frontend/src/app/services/dialog.service.ts
@@ -1,20 +1,45 @@
 import { Injectable } from '@angular/core';
 import { Dialog } from '@capacitor/dialog';
 import { from, map, Observable } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DialogService {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
-  showPrompt(title: string, message: string): Observable<string> {
+  showPrompt(title: string, message: string): Observable<string | null> {
     return from(Dialog.prompt({
-        title,
-        message
-    })).pipe(map(result => {
-        return result.value;
-    }));
-  };
+      title,
+      message
+    })).pipe(
+      map(result => result.value)
+    );
+  }
+
+  getUserIdFromEmail(title: string, message: string): Observable<number> {
+    return from(Dialog.prompt({
+      title,
+      message
+    })).pipe(
+      mergeMap(result => {
+        const email = result.value;
+        if (!email) {
+          throw new Error('No email entered');
+        }
+        try {
+          return this.userService.getUserIdFromEmail(email);
+        }
+        catch (e) {
+          alert('This email does not exist in our database');
+          throw new Error('Failed to fetch user ID');
+        }
+        
+      })
+    );
+  }
+
 }
--- team-git-it-done-frontend/src/app/services/user.service.ts
+++ team-git-it-done-frontend/src/app/services/user.service.ts
@@ -2,34 +2,50 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 import { User } from '../models/user';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
   baseURL: string = 'http://localhost:5072/api/Auth';
 
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  signUp(newUser: User){
+  signUp(newUser: User) {
     return this.http.post(`${this.baseURL}/register`, newUser);
-}
+  }
 
-login(email: string, password: string){
-  let queryParams = new HttpParams();
-  queryParams = queryParams.append('email', email);
-  queryParams = queryParams.append('password', password);
+  login(email: string, password: string) {
+    let queryParams = new HttpParams();
+    queryParams = queryParams.append('email', email);
+    queryParams = queryParams.append('password', password);
 
-  return this.http.get(`${this.baseURL}/login`,  { params: queryParams, responseType: 'text' })
-    .pipe(tap((response: any) => {
-      localStorage.setItem('myBillToken', response);
-    }));
-}
+    return this.http
+      .get(`${this.baseURL}/login`, {
+        params: queryParams,
+        responseType: 'text',
+      })
+      .pipe(
+        tap((response: any) => {
+          localStorage.setItem('myBillToken', response);
+        })
+      );
+  }
 
-// getUserDetails(): Observable<any> {
-//   return this.http.get(`${this.baseURL}/Bill/user`);
-// }
+  getUserIdFromEmail(email: string): Observable<number> {
+    return this.http
+      .get<number>(`${this.baseURL}/user`, { params: { email } })
+      .pipe(
+        catchError((error) => {
+          alert('This email does not exist in our database');
+          return throwError(() => new Error('Failed to fetch user ID'));
+        })
+      );
+  }
 
+  getUserById(userId: number): Observable<User> {
+    return this.http.get<User>(`${this.baseURL}/user/${userId}`);
+  }
 }
