2532a3a592eab791aee220895d5d37cb7fde78ae
Zian Jacobsen <25855271@sun.ac.za>
2025-04-29 10:14:48 +0200

    Fixed how the navigation works for signing in and up

--- team-git-it-done-frontend/src/app/sign-in/sign-in.page.ts
+++ team-git-it-done-frontend/src/app/sign-in/sign-in.page.ts
@@ -27,4 +27,8 @@ export class SignInPage implements OnInit {
         this.router.navigateByUrl('/sign-in');
     });
   }
+
+  navigateToSignUp() {
+    this.router.navigateByUrl('/sign-up');
+    }
 }
