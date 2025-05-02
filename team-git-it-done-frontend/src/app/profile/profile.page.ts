3b34224f355fba31d2b604922ed8e71d12cdc5c3
Medgar24 <ALANADAZE@GMAIL.COM>
2025-05-01 19:36:04 -0500

    added mock friends list

--- team-git-it-done-frontend/src/app/profile/profile.page.ts
+++ team-git-it-done-frontend/src/app/profile/profile.page.ts
@@ -9,43 +9,49 @@ import { User } from '../models/user';
   standalone: false,
 })
 export class ProfilePage implements OnInit {
-
-   img: string = '';
+  img: string = '';
 
   fullName: string = '';
   constructor(private userService: UserService) {}
 
   ngOnInit() {
-  
-
-
-this.loadProData();
+    this.loadProData();
   }
-loadProData(){
-  
-  this.fullName = localStorage.getItem('fullName') || '';
-  this.img = localStorage.getItem('img') || 'https://i.pinimg.com/originals/c0/9b/6d/c09b6d7edb7b4b89b382aa6ca0a761de.jpg';
-
-
+  loadProData() {
+    this.fullName = localStorage.getItem('fullName') || '';
+    this.img =
+      localStorage.getItem('img') ||
+      'https://i.pinimg.com/originals/c0/9b/6d/c09b6d7edb7b4b89b382aa6ca0a761de.jpg';
   }
 
-onFile(event: any){
-  const file = event.target.files[0];
-  const reader = new FileReader();
+  onFile(event: any) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
 
-  reader.onload = () => {
-    this.img = reader.result as string; // update preview
-    localStorage.setItem('img', this.img );
-    this.UpdateProPic(); // to backened
-  };
+    reader.onload = () => {
+      this.img = reader.result as string; // update preview
+      localStorage.setItem('img', this.img);
+      this.UpdateProPic(); // to backened
+    };
 
-  reader.readAsDataURL(file);
-}
-UpdateProPic() {
-  const userId = localStorage.getItem('userId');
-  this.userService.UpdateProPic(Number(userId), this.img).subscribe(response => {
-    this.img = response.img;
-  });
-}
+    reader.readAsDataURL(file);
+  }
+  UpdateProPic() {
+    const userId = localStorage.getItem('userId');
+    this.userService
+      .UpdateProPic(Number(userId), this.img)
+      .subscribe((response) => {
+        this.img = response.img;
+      });
+  }
+
+
+  triggerFileInput() {
+    const fileInput = document.querySelector<HTMLInputElement>("#fileInput");
+    if (fileInput) {
+      fileInput.click();
+    }
+  }
   
+
 }
